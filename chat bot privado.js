54da50dd1ba4ce4e04152e0a8154df3dbaa58f1d
kauajupps <kaua.juppa@produza.ind.br>
2026-06-17 10:53:39 -0300

    Update chat bot privado.js

--- chat bot privado.js
+++ chat bot privado.js
@@ -1,5 +1,13 @@
 function avisoMensal() { 
- 
+  const hoje = new Date();
+  const diaDaSemana = hoje.getDay();
+  const diaDoMes = hoje.getDate();
+  const diaDaSemanaDoPrimeiroDia = new Date(hoje.getFullYear(), hoje.getUTCMonth(), 1).getDay();
+
+  // Calcula o índice da semana no mês
+  const semanaDoMes = Math.ceil((diaDoMes + diaDaSemanaDoPrimeiroDia) / 7);
+
+  if(diaDaSemana==2 && semanaDoMes==2){
   // Lista com as URLs dos webhooks de cada espaço individual
   var put = PropertiesService.getScriptProperties().getProperty('teste');
   user = put.split(",");
@@ -10,5 +18,7 @@ function avisoMensal() {
     var options = { "method": "post", "contentType": "application/json", "payload": payload };
     UrlFetchApp.fetch(user[i], options);
   };
-  
+  }
+  else{return;};
 }
+
